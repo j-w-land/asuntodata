003e89a1a0a286c41820c12e2a60acdf2fa015fe
j-w-land <jiiweemail@gmail.com>
2021-01-11 10:39:49 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import './App.css';
-import Table from './Table.js';
+import React from "react";
+import "./App.css";
+import Table from "./Table.js";
 
 function App() {
-
-  fetch('53850.json', { headers : {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    }
-  }).then(response => response.json());
+  fetch("53850.json", {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  }).then((response) => response.json());
 
   return (
-      <div className="App">
-        <ul id="Navbar">
-          <li><h3>Kauppahinnat.fi</h3></li>
-        </ul>
+    <div className="App">
+      <ul id="Navbar">
+        <li>
+          <h3>Kauppahinnat.fi</h3>
+        </li>
+      </ul>
 
-        <br />
-        <Table />>
-      </div>
-    );
-  }
+      <br />
+      <Table />
+    </div>
+  );
+}
 
 export default App;
