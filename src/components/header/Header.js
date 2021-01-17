52b989cddeaa74fe8f6a772e353335ea15dbbb00
j-w-land <jiiweemail@gmail.com>
2021-01-17 19:03:48 +0200

    Search-toiminnon lisäys

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,10 +3,56 @@ import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import ListGroup from "react-bootstrap/ListGroup";
+
+import InputGroup from "react-bootstrap/InputGroup";
 import Button from "react-bootstrap/Button";
+import { Dropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import Search from "./Search";
+import { useState } from "react";
 
 export default function Header() {
+  let history = useHistory();
+  const [searchValue, setSearchValue] = useState("");
+  const [searchFormActive, setSearchFormActive] = useState(false);
+  const [searchResults, setSearchResults] = useState([]);
+  const [searching, setSearching] = useState(false);
+
+  const handleSearchForm = (e) => {
+    try {
+      setSearchValue(e.target.value);
+      setSearching(true);
+    } catch (error) {
+      setSearchValue("");
+      setSearching(false);
+    }
+  };
+
+  const handleSearchFormBlur = (e) => {
+    let idString = ["init"];
+
+    try {
+      let id = e.relatedTarget.id;
+      idString = id.split(":");
+    } catch (error) {}
+
+    setSearchFormActive(false);
+    setSearchValue("");
+
+    if (idString[0] == "headerLink") {
+      history.push(idString[1]);
+    } /* else {
+        
+      } */
+  };
+
+  const handleSearchFormClick = (e) => {
+    setSearchFormActive(true);
+  };
+
   return (
     <div>
       <Navbar bg="light" expand="lg">
@@ -20,7 +66,6 @@ export default function Header() {
               Etusivu
             </Nav.Link>
 
-            {/* <Nav.Link href="#link">Link</Nav.Link> */}
             <NavDropdown title="Näkymät" id="basic-nav-dropdown">
               <NavDropdown.Item as={Link} to="/postinumero/53850">
                 53850
@@ -37,10 +82,59 @@ export default function Header() {
               </NavDropdown.Item> */}
             </NavDropdown>
           </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Etsi" className="mr-sm-2" />
-            <Button variant="outline-success">Etsi</Button>
-          </Form>
+
+          <div>
+            <Form inline>
+              <FormControl
+                onChange={handleSearchForm}
+                type="text"
+                placeholder="Etsi kaupunki tai postinumero..."
+                className="mr-sm-2"
+                value={searchValue}
+                onBlur={handleSearchFormBlur}
+                onClick={handleSearchFormClick}
+              />
+
+              <Search
+                searchValue={searchValue}
+                setSearchResults={setSearchResults}
+                setSearching={setSearching}
+              />
+            </Form>
+
+            {searchFormActive && searching && (
+              <ListGroup id="searchResultList">
+                {searching == true ? (
+                  <ListGroup.Item key={"searchResult-searching"}>
+                    Hakee
+                  </ListGroup.Item>
+                ) : null}
+              </ListGroup>
+            )}
+
+            {searchFormActive && !searching && (
+              <ListGroup id="searchResultList">
+                {searchResults == null ? (
+                  <ListGroup.Item key={"searchResult-none"}>
+                    Ei tuloksia
+                  </ListGroup.Item>
+                ) : (
+                  Object.keys(searchResults).map((key) =>
+                    searchResults[key].map((e, i) => (
+                      <ListGroup.Item
+                        as={Link}
+                        id={`headerLink:/${key.toLowerCase()}/${e}`}
+                        to={`/${key.toLowerCase()}/${e}`}
+                        key={"searchResult-" + i}
+                      >
+                        {key + " : " + e}
+                      </ListGroup.Item>
+                    ))
+                  )
+                )}
+              </ListGroup>
+            )}
+          </div>
         </Navbar.Collapse>
       </Navbar>
     </div>
---
+++ src/components/header/Search.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import zipCodeStructure from "../../assets/data/zipCodeStructure.json";
+
+export default function Search({
+  searchValue,
+  setSearchResults,
+  setSearching,
+}) {
+  useEffect(() => {
+    if (searchValue == "") {
+      setSearchResults([]);
+      setSearching(false);
+      return null;
+    }
+
+    let matchesCity = zipCodeStructure.data.filter((e) =>
+      e.kaupunki.toLowerCase().includes(searchValue.toLowerCase())
+    );
+
+    let cities = [];
+    for (const city in matchesCity) {
+      cities.push(matchesCity[city].kaupunki);
+    }
+    cities = [...new Set(cities)];
+
+    let matchesZip = zipCodeStructure.data.filter((e) =>
+      e.postinumero.includes(searchValue)
+    );
+
+    let zips = [];
+    for (const zip in matchesZip) {
+      zips.push(matchesZip[zip].postinumero);
+    }
+    zips = [...new Set(zips)];
+
+    if (cities.length === 0 && zips.length === 0) {
+      setSearchResults(null);
+    } else {
+      setSearchResults({ Kaupunki: cities, Postinimero: zips });
+    }
+    setSearching(false);
+  }, [searchValue]);
+
+  return null;
+}
